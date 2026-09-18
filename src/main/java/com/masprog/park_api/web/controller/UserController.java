a1c8c0531a9c62a33b4cfa5dd803be0100becd1b
Mauro-Manuel <mauromanuel050@gmail.com>
2026-09-19 00:52:33 +0100

    feat: add feature to get user by id

--- src/main/java/com/masprog/park_api/web/controller/UserController.java
+++ src/main/java/com/masprog/park_api/web/controller/UserController.java
@@ -5,10 +5,7 @@ import com.masprog.park_api.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,4 +19,12 @@ public class UserController {
        User userSave = userService.save(user);
        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable Long id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+
 }
