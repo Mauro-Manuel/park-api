ee527db0e545785b8d68cc13789afd6c9e643954
Mauro-Manuel <mauromanuel050@gmail.com>
2026-09-19 00:22:33 +0100

    feat: Add feature to create a new user

--- src/main/java/com/masprog/park_api/service/UserService.java
+++ src/main/java/com/masprog/park_api/service/UserService.java
@@ -1,12 +1,19 @@
 package com.masprog.park_api.service;
 
+import com.masprog.park_api.entity.User;
 import com.masprog.park_api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 }
